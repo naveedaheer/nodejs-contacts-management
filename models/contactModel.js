eb441b6107a178246329dab3d6aaaf5cace2cb0e
Naveed Aheer <naveedaheer@gmail.com>
2024-01-17 17:51:24 +0500

    Updated code

--- models/contactModel.js
+++ models/contactModel.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, Sequelize) => {
         lastName: {
             type: Sequelize.STRING
         },
-        Email: {
+        email: {
             type: Sequelize.STRING
         },
         phoneNumber: {
