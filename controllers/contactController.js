9ae2ce56e107f82808e8888ba80d928b04d7eadd
Naveed Aheer <naveedaheerpc@gmail.com>
2024-01-20 18:58:35 +0500

    Implemented pagination and filters for contacts

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,61 +1,65 @@
-const { Op, where } = require('sequelize');
-const db = require('../models')
-const contactDb = db.contacts
+const { Op } = require('sequelize');
+const db = require('../models');
+const contactDb = db.contacts;
 
 const addContact = async (req, res) => {
     await contactDb.create(req.body)
     res.status(200).send("Contact Created successfully!")
 }
 
-const getAllContacts = async (req, res) => {
-    const { firstName, lastName, pageNumber, pageSize } = req.query;
-    let whereCondition = {};
-
-    if (firstName && lastName) {
-        whereCondition = {
-            firstName,
-            lastName,
-        };
-    } else if (firstName || lastName) {
-        whereCondition = {
-            [Op.or]: [
-                { firstName },
-                { lastName },
-            ].filter(condition => condition[Object.keys(condition)[0]] !== undefined),
-        };
-    } else {
-        whereCondition = {};
-    }
-
-    const offset = (pageNumber - 1) * pageSize;
-    const data = await contactDb.findAll({
-        where: whereCondition,
-        limit: parseInt(pageSize),
-        offset: parseInt(offset),
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).send({ error: 'Internal Server Error' });
-    });
-
-    const totalContacts = await contactDb.count({
-        where: whereCondition,
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).send({ error: 'Internal Server Error' });
-    });
-
-    const totalPages = Math.ceil(totalContacts / parseInt(pageSize));
-
-    res.status(200).send({
-        data,
-        pageInfo: {
-            pageSize: parseInt(pageSize),
-            currentPage: parseInt(pageNumber),
-            totalContacts,
-            totalPages,
-        },
-    });
+const handleErrors = (res, status, message) => {
+    console.error(message);
+    res.status(status).send({ error: message });
 };
+
+const getSequelizeOptions = (pageNumber, pageSize, whereCondition) => ({
+    where: whereCondition,
+    limit: parseInt(pageSize),
+    offset: (parseInt(pageNumber) - 1) * parseInt(pageSize),
+});
+
+const getAllContacts = async (req, res) => {
+    try {
+        const { firstName, lastName, pageNumber, pageSize } = req.query;
+
+        // Basic validation for pageNumber and pageSize
+        if (!pageNumber || !pageSize) {
+            return handleErrors(res, 400, 'Invalid pageNumber or pageSize');
+        }
+
+        let whereCondition = {};
+
+        if (firstName) {
+            whereCondition.firstName = {
+                [Op.iLike]: `%${firstName}%`,
+            };
+        }
+
+        if (lastName) {
+            whereCondition.lastName = {
+                [Op.iLike]: `%${lastName}%`,
+            };
+        }
+
+        const data = await contactDb.findAll(getSequelizeOptions(pageNumber, pageSize, whereCondition));
+        const totalContacts = await contactDb.count({ where: whereCondition });
+
+        const totalPages = Math.ceil(totalContacts / parseInt(pageSize));
+
+        res.status(200).send({
+            data,
+            pageInfo: {
+                pageSize: parseInt(pageSize),
+                currentPage: parseInt(pageNumber),
+                totalContacts,
+                totalPages,
+            },
+        });
+    } catch (err) {
+        handleErrors(res, 500, 'Internal Server Error');
+    }
+};
+
 const updateContact = async (req, res) => {
     const id = req.params.id
     const updateContact = await contactDb.update(req.body, { where: { id } })
