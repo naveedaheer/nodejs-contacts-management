cd958315ac8343b3d5a30828ee10a42e855bc6f0
Naveed Aheer <naveedaheerpc@gmail.com>
2024-01-20 15:29:08 +0500

    Added DATABASE_URL in config

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    HOST: process.env.DB_HOST,
-    USER: process.env.DB_USER,
-    PASSWORD: process.env.DB_PASSWORD,
-    DB_NAME: process.env.DB_NAME,
+    // HOST: process.env.DB_HOST,
+    // USER: process.env.DB_USER,
+    // PASSWORD: process.env.DB_PASSWORD,
+    // DB_NAME: process.env.DB_NAME,
+    DATABASE_URL: process.env.DATABASE_URL,
     dialect: "postgres",
     dialectOptions: {
         ssl: {
