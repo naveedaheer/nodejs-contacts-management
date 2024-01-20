987036d657bb09e8a9711f2689038dcb54ba4d3c
Naveed Aheer <naveedaheerpc@gmail.com>
2024-01-20 15:27:52 +0500

    Connected Postgres DB

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB_NAME, dbConfig.USER, dbConfig.PASSWORD, {
+const sequelize = new Sequelize(dbConfig.DATABASE_URL, {
     host: dbConfig.HOST,
     dialect: dbConfig.dialect,
     operatorsAliases: false,
